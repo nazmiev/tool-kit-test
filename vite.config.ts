4468b32431ee20f21c87a8c05d83f99bc07a838d
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-07 12:50:07 +0700

    deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+const base = process.env.NODE_ENV === 'production' && process.env.REPO_NAME
+    ? '/' + process.env.REPO_NAME+ '/'
+    : '/';
+
 export default defineConfig({
+  base: base,
   plugins: [react()],
 })

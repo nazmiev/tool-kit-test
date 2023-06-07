ff2507730fb9068de78d8baf994f914a80496eae
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-07 13:33:09 +0700

    token env test

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import tokenTest from './utils/utils'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
+      <p>{tokenTest}</p>
       <button onClick={() => setCount((count) => count + 1)}>
         count is {count}
       </button>
---
+++ src/utils/utils.ts
@@ -0,0 +1,4 @@
+const TOKEN = import.meta.env.VITE_TOKEN;
+
+
+export default const tokenTest = TOKEN;

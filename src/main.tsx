995c545b387af591ee5adf20ef010e36de419093
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-08 14:21:06 +0700

    js вместо ts

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 // import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import App from './App.jsx'
 import './index.css'
 import { Provider } from 'react-redux/es/exports'
 import store from "./redux/store.js";

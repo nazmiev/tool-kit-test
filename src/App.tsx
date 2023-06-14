d9a5f74bd7df8254a6ce6e2ba7021a7d34927845
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-14 07:07:21 +0700

    проверка деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import Home from './pages/Home.tsx';
 // import RepositoryPage from './pages/RepositoryPage/';
 import MainLayout from './layouts/MainLayout.tsx';
 

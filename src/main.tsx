8067013268ee2ccf427b1cb5e1c8e35d5b16f18f
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-14 12:00:34 +0700

    проверка деплоя

--- src/main.tsx
+++ src/main.tsx
@@ -6,11 +6,16 @@ import { Provider } from 'react-redux/es/exports'
 import { BrowserRouter } from 'react-router-dom'
 import { store } from "./redux/store.ts";
 
+const baseName = import.meta.env.BASE_URL;
+
+console.log('BASE_URL', baseName);
+
 const rootElem = document.getElementById('root');
 
 if (rootElem) {
   ReactDOM.createRoot(rootElem as HTMLElement).render(
-    <BrowserRouter basename="/tool-kit-test">
+    // <BrowserRouter basename={base}>
+    <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const base = process.env.NODE_ENV === 'production' && process.env.REPO_NAME
-    ? '/' + process.env.REPO_NAME+ '/'
-    : '/';
+  ? '/' + process.env.REPO_NAME + '/'
+  : '/';
 
 export default defineConfig({
   base: base,

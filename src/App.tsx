25e28d71b9ad9407f480ba795a3518d07095e84f
Роман Назмиев <roma.nazmiev@gmail.com>
2023-06-14 11:24:05 +0700

    проверка деплоя

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,18 @@
-// import { Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home.tsx';
-// import RepositoryPage from './pages/RepositoryPage/';
-// import MainLayout from './layouts/MainLayout.tsx';
+import RepositoryPage from './pages/RepositoryPage/';
+import MainLayout from './layouts/MainLayout.tsx';
 
 function App() {
   console.log('app');
   return (
     <>
-    <Home />
-      {/* <Routes>
-        <Route path="/" element={<MainLayout />}>
+      <Routes>
+        <Route path="/tool-kit-test/" element={<MainLayout />}>
           <Route path="" element={<Home />} />
           <Route path="/repository/:owner/:reponame" element={<RepositoryPage />} />
         </Route>
-      </Routes> */}
+      </Routes>
     </>
   );
 }
